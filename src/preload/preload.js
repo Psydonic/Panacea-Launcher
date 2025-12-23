d6d1570a1d12dbe10754eff4f9439e00c542ada2
BB <benbellavia123@gmail.com>
2025-12-23 00:21:40 +0000

    feat: Expose ipcRenderer.send to the renderer process

--- src/preload/preload.js
+++ src/preload/preload.js
@@ -1,6 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
+  send: (channel, data) => ipcRenderer.send(channel, data),
   onStatus: (callback) => ipcRenderer.on('status', callback),
   onProgress: (callback) => ipcRenderer.on('progress', callback),
   onError: (callback) => ipcRenderer.on('error', callback)

c59cd3d63ec6f095103f844a98367a3d5551265c
BB <benbellavia123@gmail.com>
2025-12-23 00:50:46 +0000

    Revert "feat: add ipc handlers and update preload script"
    This reverts commit d8fb0931e934571230763bab2f1cef11ef94177c.

--- src/preload/preload.js
+++ src/preload/preload.js
@@ -1,7 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron');
 
 contextBridge.exposeInMainWorld('electronAPI', {
-  send: ({ type, payload }) => ipcRenderer.invoke(type, payload),
+  send: (channel, data) => ipcRenderer.send(channel, data),
   onStatus: (callback) => ipcRenderer.on('status', callback),
   onProgress: (callback) => ipcRenderer.on('progress', callback),
   onError: (callback) => ipcRenderer.on('error', callback)
